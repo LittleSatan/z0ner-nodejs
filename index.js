9f159c5b08fcc7fbf487b22455e6f5866ab1d686
marissa <leow.mannheim@gmail.com>
2017-06-25 20:12:17 +0000

    Server is working now ig... Now only API, database and frontend is missing... pls kill me q.q

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-'use strict';
+console.log(`Starting server...`);
 
-console.log("Starting server...");
-
-const express = require('express');
+const express = require(`express`);
 const app = express();
+const ejs = require('ejs');
 
-app.use(express.static(__dirname + '/public'));
+// express - static files
+app.use(`/static`, express.static(__dirname + `/public/`));
 
-app.get('*', function(req, res) {
-    res.redirect('/');
+// express - send favicon
+app.get(`/favicon.ico`, function(req, res){
+    res.send(__dirname + `/public/favicon.ico`);
+});
+
+// express - send user to id
+app.get(`/:id`, function(req, res){
+    
+    let id = req.params.id;
+    
+    // check if id is valid
+    if (isNaN(id) || !(id == parseInt(id, 10)) || id < 0){
+        redToRandomID(req, res);
+        return;
+    }
+    openID(req, res, req.params.id)}
+
+);
+
+// express - user tried to open index or something invalid. open random id
+app.get(`*`, function(req, res) {
+    redToRandomID(req, res);
 });
 
 let server = app.listen(process.env.PORT, process.env.IP);
+
+function openID(req, res, id){
+    app.engine('ejs', ejs.renderFile);
+    res.render(__dirname + `/views/index.ejs`, {userid: id});
+}
+
+function redToRandomID(req, res){
+    let id = Math.floor(Math.random() * 100);
+    res.redirect(`/${id}`);
+}
