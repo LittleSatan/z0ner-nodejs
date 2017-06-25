df6760e747126b52908d3a464a367b315d90057b
marissa <leow.mannheim@gmail.com>
2017-06-25 15:31:19 +0000

    its running now ._.

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ console.log("Starting server...");
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res) {
-    res.redirect(__dirname + 'public/html/index.html');
+    res.redirect('/');
 });
 
 let server = app.listen(process.env.PORT, process.env.IP);
