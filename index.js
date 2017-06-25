770cded40cafd9195ec7576ab6c490e9ca6e3322
marissa <leow.mannheim@gmail.com>
2017-06-25 14:54:59 +0000

    bla

--- index.js
+++ index.js
@@ -11,8 +11,4 @@ app.get('*', function(req, res) {
     res.redirect(__dirname + 'public/html/index.html');
 });
 
-app.set('port', process.env.PORT || 3000);
-app.set('domain', process.env.IP);
-
-console.log(`Running Server on ${app.get('port')} and ${app.get('domain')}`);
-let server = app.listen();
+let server = app.listen(process.env.PORT, process.env.IP);
